567ce8e9f755131ac7ec76e6ce7c62163825f609
Javier Alvarez <javier@crowsoft.com.ar>
2012-09-19 06:41:36 -0300

    Add a template for postgresql functions

---
+++ CSScript/postgresql/functions/empty_function_template.sql
@@ -0,0 +1,11 @@
+﻿CREATE OR REPLACE FUNCTION function_name (OUT/IN parameter_name data_type, ...)
+RETURNS void/data_type AS
+$BODY$
+DECLARE
+	var_name data_type;
+BEGIN
+
+END;
+$BODY$
+  LANGUAGE plpgsql
+;
