cad61bfd00652c6d2d851fa11deaa85060160d88
Javier Alvarez <javier@crowsoft.com.ar>
2014-05-29 15:35:39 -0300

    Implementen navigation for Usuario

--- web/cairo/app/models/cairo/Router.scala
+++ web/cairo/app/models/cairo/Router.scala
@@ -2,6 +2,20 @@ package models.cairo
 
 case class RouterEntry(fileHandler: String, action: String, path: String, action2: String, path2: String) {
   val handlerObject = fileHandler.substring(fileHandler.indexOf("/") + 2)
+
+  val pathForNavigate = forNavigate(path)
+  val path2ForNavigate = forNavigate(path2)
+
+  val pathParam = param(path)
+  val path2Param = param(path2)
+
+  def param(path: String) = {
+    if(path.contains(":")) "id" else ""
+  }
+
+  def forNavigate(path: String) = {
+    if(path.contains(":")) s""""${path.substring(0, path.indexOf(":"))}" + id""" else s""""$path/""""
+  }
 }
 
 object Router {
